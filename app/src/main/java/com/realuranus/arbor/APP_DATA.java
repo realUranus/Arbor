014eddff4d98792b7a76cbd43958ac9d355a5fc7
realUranus <theodorusan@163.com>
2019-05-05 19:03:12 +0800

    ListView

--- app/src/main/java/com/realuranus/arbor/APP_DATA.java
+++ app/src/main/java/com/realuranus/arbor/APP_DATA.java
@@ -1,8 +1,12 @@
 package com.realuranus.arbor;
 
 public class APP_DATA {
-    private final static String BASE_URL = "https://www.lintcode.com/";
+    //url
+    private final static String BASE_URL = "https://www.nowcoder.com/ta/leetcode";
     private final static String PROBLEM_URL = BASE_URL + "problem";
+
+    //selector
+    private final static String PROBLEM_SELECTOR = ".nk-content table a";
 
     public static String getBASE_URL() {
         return BASE_URL;
@@ -10,5 +14,9 @@ public class APP_DATA {
 
     public static String getProblemUrl() {
         return PROBLEM_URL;
+    }
+
+    public static String getProblemSelector() {
+        return PROBLEM_SELECTOR;
     }
 }
--- app/src/main/java/com/realuranus/arbor/ArborActivity.java
+++ app/src/main/java/com/realuranus/arbor/ArborActivity.java
@@ -5,6 +5,8 @@ import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.orhanobut.logger.AndroidLogAdapter;
 import com.orhanobut.logger.Logger;
@@ -15,9 +17,12 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ArborActivity extends AppCompatActivity {
     private final String TAG = "ArborActivity";
+    private ArrayList<String> problem_data = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,26 +31,29 @@ public class ArborActivity extends AppCompatActivity {
 
         Logger.addLogAdapter(new AndroidLogAdapter());
 
-        SharedPreferences sharedPreferences = this.getPreferences(Context.MODE_PRIVATE);
+        problem_data.add("One");
+        problem_data.add("Two");
+
+//        SharedPreferences sharedPreferences = this.getPreferences(Context.MODE_PRIVATE);
 //        sharedPreferences.getString(getString(R.string.app_launch_time), Context.MODE_PRIVATE);
 
         new Thread(){
             @Override
             public void run() {
-//                super.run();
                 try {
-                    Document document = Jsoup.connect((APP_DATA.getProblemUrl())).get();
+                    Document document = Jsoup.connect((APP_DATA.getBASE_URL())).get();
                     String title = document.title();
-                    Elements links = document.select("a[href]");
+                    Elements links = document.select(APP_DATA.getProblemSelector());
                     Logger.t(TAG).i("links:");
                     for (Element e : links){
-                        Logger.t(TAG).i(e.tagName() + e.text());
+                        problem_data.add(e.text());
                     }
-//                    String body = document.body();
-//                    Logger.t(TAG).i();
                 }catch (IOException e){
                 }
             }
         }.start();
+
+        ListView problem_lv = (ListView)findViewById(R.id.problem_list);
+        problem_lv.setAdapter(new ArrayAdapter<String>(this, R.layout.problem_item,problem_data));
     }
 }
