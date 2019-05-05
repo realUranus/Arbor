5d58b4d6aa9eb255f7b74e6848bc03af51edcbc3
realUranus <theodorusan@163.com>
2019-05-05 11:17:32 +0800

    import logger

--- app/src/main/java/com/realuranus/arbor/ArborActivity.java
+++ app/src/main/java/com/realuranus/arbor/ArborActivity.java
@@ -1,8 +1,13 @@
 package com.realuranus.arbor;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+
+import com.orhanobut.logger.AndroidLogAdapter;
+import com.orhanobut.logger.Logger;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -17,13 +22,23 @@ public class ArborActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_arbor);
 
-        try {
-            Document document = Jsoup.connect("https://www.lintcode.com/").get();
-            String title = document.title();
-            Log.d(TAG, title);
-        }catch (IOException e){
+        Logger.addLogAdapter(new AndroidLogAdapter());
 
-        }
+        SharedPreferences sharedPreferences = this.getPreferences(Context.MODE_PRIVATE);
+//        sharedPreferences.getString(getString(R.string.app_launch_time), Context.MODE_PRIVATE);
+
+        new Thread(){
+            @Override
+            public void run() {
+//                super.run();
+                try {
+                    Document document = Jsoup.connect("https://www.lintcode.com/").get();
+                    String title = document.title();
+                    Logger.t(TAG).i(title);
+                }catch (IOException e){
+                }
+            }
+        }.start();
 
     }
 }
